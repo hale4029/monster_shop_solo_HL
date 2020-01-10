daa6007e1b5f667b4871ebd86413898aca4c2801
Harrison Levin <harrison483@gmail.com>
2020-01-10 16:07:35 -0700

    index coupon route - merchant

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,7 @@ Rails.application.routes.draw do
   namespace :merchant  do
     get '/', to: 'dashboard#show'
     resources :orders, only: [:show, :update]
+    resources :coupons, only: [:index]
     resources :items, only: [:index, :show, :update, :destroy, :new, :create, :edit]
   end
 
