eb2b1fe77f8d6f43a16d5d181afb10d11525a2d6
Harrison Levin <harrison483@gmail.com>
2020-01-10 18:08:08 -0700

    change file name

---
+++ spec/features/merchant/coupons/index_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Coupon Show Page" do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    #@item_1 = create(:item, merchant: @merchant)
+    #@item_2 = create(:item, merchant: @merchant)
+    @coupon_1 = create(:coupon, merchant_id: @merchant_1.id)
+    @coupon_2 = create(:coupon, merchant_id: @merchant_1.id)
+    @coupon_3 = create(:coupon, merchant_id: @merchant_2.id)
+
+    @merchant_employee = create(:user, role: 1, merchant: @merchant_1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_employee)
+  end
+
+  it "access merchant coupon show page" do
+    visit merchant_path
+    click_on "Manage Coupons"
+    expect(current_path).to eq(merchant_coupons_path)
+  end
+
+  it "show all coupons for only the merchant associated with the merch_employee" do
+    visit merchant_coupons_path
+
+    within "#coupon-#{@coupon_1.id}" do
+      expect(page).to have_content(@coupon_1.name)
+      expect(page).to have_content(@coupon_1.code)
+      expect(page).to have_content(@coupon_1.discount)
+      expect(page).to have_content(@coupon_1.merchant_id)
+      #click_link("Order Number: #{@order.id}")
+    end
+
+    within "#coupon-#{@coupon_2.id}" do
+      expect(page).to have_content(@coupon_2.name)
+      expect(page).to have_content(@coupon_2.code)
+      expect(page).to have_content(@coupon_2.discount)
+      expect(page).to have_content(@coupon_2.merchant_id)
+      #click_link("Order Number: #{@order.id}")
+    end
+
+    #sad-test
+    expect(page).to_not have_css("#coupon-#{@coupon_3.id}")
+    expect(page).to_not have_content(@coupon_3.code)
+  end
+
+
+end
