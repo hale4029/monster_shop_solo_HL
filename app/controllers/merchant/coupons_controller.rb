92df335f01c9d0c6cdeabf9125ec74177913e81c
Harrison Levin <harrison483@gmail.com>
2020-01-10 18:07:47 -0700

    controller - index

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -1,5 +1,5 @@
 class Merchant::CouponsController < Merchant::BaseController
   def index
-    #@coupons = Coupon.where(merchant_id: current_user.merchant_id)
+    @coupons = Coupon.where(merchant_id: current_user.merchant_id)
   end
 end
