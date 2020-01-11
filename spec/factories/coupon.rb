c80cb9c48190d1fb032e2d6ecbc00bcbf8a97897
Harrison Levin <harrison483@gmail.com>
2020-01-10 18:08:28 -0700

    update factory

--- spec/factories/coupon.rb
+++ spec/factories/coupon.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :coupon do
     sequence(:name) { |n| "Discount: #{n}"}
     code { SecureRandom.hex(10) }
-    discount { rand(0.00..1.00).round(2) }
+    discount { rand(1..100) }
     merchant
   end
 end
