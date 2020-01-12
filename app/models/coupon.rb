98153f30f10456728523626bcbd66fe171a74e19
Harrison Levin <harrison483@gmail.com>
2020-01-11 20:28:05 -0700

    coupon method

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -10,4 +10,8 @@ class Coupon < ApplicationRecord
   validates :code, uniqueness: true, presence: true
   validates :name, uniqueness: true, presence: true
   validates_inclusion_of :discount, in: 1..100
+
+  def self.coupon_lookup(code)
+    where(code: code).first
+  end
 end
