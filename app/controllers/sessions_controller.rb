2a777130a485d6483de79f1ab1c2a93e9418763e
Harrison Levin <harrison483@gmail.com>
2020-01-13 08:38:11 -0700

    delete coupon session at logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -37,6 +37,7 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     session.delete(:cart)
+    session.delete(:coupon)
     flash[:success] = "Goodbye, you are now logged out."
     redirect_to '/'
   end
